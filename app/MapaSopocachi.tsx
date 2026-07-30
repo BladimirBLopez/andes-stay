8597567a65d3f7abf05398a939994aa0b4b97e4d
BladimirBLopez <blopezbladimir@gmail.com>
2026-07-30 14:37:28 -0400

    Cambiar estilo del mapa a OpenStreetMap estándar (colorido, con nombres de calles)

--- app/MapaSopocachi.tsx
+++ app/MapaSopocachi.tsx
@@ -55,8 +55,8 @@ export default function MapaSopocachi() {
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={SOPOCACHI} icon={icon}>
           <Popup>VIP Estadías — Sopocachi, La Paz</Popup>
